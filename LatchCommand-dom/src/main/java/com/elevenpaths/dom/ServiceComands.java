30f45b6cc0123c70accbfc0caae479a0fd98cc7f
Matias Nahuel Heredia <matias@informaticos.com>
2014-12-07 20:09:06 -0300

    add ServiceCommands class domain

---
+++ LatchCommand-dom/src/main/java/com/elevenpaths/dom/ServiceComands.java
@@ -0,0 +1,27 @@
+package com.elevenpaths.dom;
+
+public class ServiceComands {
+
+	private String latchId;
+	private String command;
+	private boolean enabled;
+	public String getLatchId() {
+		return latchId;
+	}
+	public void setLatchId(String latchId) {
+		this.latchId = latchId;
+	}
+	public String getCommand() {
+		return command;
+	}
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+}
