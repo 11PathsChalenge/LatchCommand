b627e873ed955f6682ece8abda37e283c7fdd390
Matias Nahuel Heredia <matias@informaticos.com>
2014-12-07 20:42:02 -0300

    add domain ServicesCommand persistentCapable

--- LatchCommand-JDO/src/main/java/org/elevenpaths/latchCommand/repository/RepoCommand.java
+++ LatchCommand-JDO/src/main/java/org/elevenpaths/latchCommand/repository/RepoCommand.java
@@ -1,6 +1,9 @@
 package org.elevenpaths.latchCommand.repository;
 
-
 public class RepoCommand {
 
+	public void createCommand (String latchId,String command)
+	{
+		
+	}
 }
--- LatchCommand-dom/src/main/java/com/elevenpaths/latchCommand/dom/ServiceComands.java
+++ LatchCommand-dom/src/main/java/com/elevenpaths/latchCommand/dom/ServiceComands.java
@@ -1,22 +1,28 @@
 package com.elevenpaths.latchCommand.dom;
 
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Column;
+@PersistenceCapable()
 public class ServiceComands {
 
 	private String latchId;
 	private String command;
 	private boolean enabled;
+	@Column(allowsNull="false")
 	public String getLatchId() {
 		return latchId;
 	}
 	public void setLatchId(String latchId) {
 		this.latchId = latchId;
 	}
+	@Column(allowsNull="false")
 	public String getCommand() {
 		return command;
 	}
 	public void setCommand(String command) {
 		this.command = command;
 	}
+	@Column(allowsNull="false")
 	public boolean isEnabled() {
 		return enabled;
 	}
